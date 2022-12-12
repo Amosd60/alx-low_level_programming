ba9c3f3334697858456c77dd0b0e1c457a4c1b7d
Amosd60 <amossunday574@gmail.com>
2022-12-11 20:14:37 -0800

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,10 @@
-#include <stdio.h>                                                                                                                      
-
-/**                                                                                                                                     
- *
- *  * main - Entry                                                                                                                         
- *
- *   * Return: Always 0                                                                                                                     
- *
- *    */                                                                                                                                     
-
-int main(void)                                                                                                                          
-
-{                                                                                                                                       
-
-	        puts("\"Programming is like building a multi lingual puzzle);                                                            
-
-			        return (0);
-
+#include <stdio.h>
+/**
+ * main-Entry
+ * Return:Always
+ */
+int main(void)
+{
+	puts("\"programmming is like building a multi lingual puzzle);
+	return(0);
 }
-
-
