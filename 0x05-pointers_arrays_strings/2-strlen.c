bdbcfb93c68158e09e5d14567a8ba85755c6e1c8
Amosd60 <amossunday574@gmail.com>
2022-12-20 20:03:15 -0800

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "maih.h"
+#include <stdio.h>
+/**
+ * main - to return the lenth of a string
+ *@str: lenth of a string
+
+ * Return: always to zero
+ */
+
+int _strlen(char *s);
+{
+	char *str
+	int len
+
+	str = my first strlen;
+	len = _strlen(str);
+	_putchar("%d\n", len);
+	return  (0);
+}
