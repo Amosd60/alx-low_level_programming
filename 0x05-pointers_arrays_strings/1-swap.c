e8027f954a9e3e6d9f6327585a984318f72929f9
Amosd60 <amossunday574@gmail.com>
2022-12-20 19:34:09 -0800

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - swap the value of twoo integers
+ * @a: value to be swap
+ * @b: value to be swap
+ *
+ * Return: always 0
+ */
+
+void swap_int(int *a, int *b);
+{
+	int *a;
+	int *b;
+
+	*a = 98;
+	*b = 42;
+	_putchar("value of '*a' , : %d\n" && "value of '*b' , : %d\n", a, b,);
+	_putchar("swap_int '*a & *b, : %d\n")
+	_putchar("value of '*a' , : %d\n" && "value of '*b' , : %d\n", a, b,);
+	return (0);
+}
