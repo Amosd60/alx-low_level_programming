7b0c4268bf7632be2c9a785c9b0ab4fbe2a4c653
Amosd60 <amossunday574@gmail.com>
2022-12-20 18:40:36 -0800

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,18 +1,21 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * reset_to_98 - resets the value of a variablr back to 98
+ * reset_to_98 - resets the value of a variable back to 98
+ *
  * @n: varieble to be reset
- * Return: void
+ *
+ * Return: Always 0
  */
 
 void reset_to_98(int *n)
 {
 	int n;
 
-	*n = 402
-	_putchar("*n=%d\n", n);
-	reset_to_98(&*n);
-	_putchar("*n=%d\n", n);
+	n = 402
+	_putchar("n=%d\n", n);
+	reset_to_98(&n);
+	_putchar("n=%d\n", n);
 	return (0);
 }
