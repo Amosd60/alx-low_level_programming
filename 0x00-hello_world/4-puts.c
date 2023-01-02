8749024fecde1f8395256b590ae84a91fb67bbcb
Amosd60 <amossunday574@gmail.com>
2023-01-01 18:03:07 -0800

    my first c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,11 @@
 /**
  * main - print a line of code using puts
  *
- * Return: 0
+ * Return: Always 0
  */
 int main(void)
 {
 	puts("\"programmming is like building a multilingual puzzle");
+
 	return (0);
 }
